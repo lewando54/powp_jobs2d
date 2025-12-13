59fcd6598a00140a56895035ead8bed5cfa22d7b
lewando54 <lewando54@gmail.com>
2025-12-13 09:25:39 +0100

    Add Command Pattern

---
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -0,0 +1,23 @@
+package edu.kis.powp.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+/**
+ * Implementation of Job2dDriverCommand for operateTo command functionality.
+ */
+public class OperateToCommand implements DriverCommand {
+
+    private int x;
+    private int y;
+
+    public OperateToCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void execute(Job2dDriver driver) {
+        driver.operateTo(x, y);
+    }
+}
+
